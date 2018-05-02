358eebc10027d11702c7b7e62e8cfd8543d4a8ab
Markus Felten <Markus.Felten@gmx.de>
2018-05-02 22:31:14 +0200

    test: code cleanup

--- tests/simple-test.js
+++ tests/simple-test.js
@@ -1,7 +1,7 @@
 const serve = require('koa-static');
 const Koa = require('koa');
 const app = new Koa();
-const path = require('path');
+const { join } = require('path');
 const makeDir = require('make-dir');
 const puppeteer = require('puppeteer');
 const looksSame = require('looks-same');
@@ -10,7 +10,7 @@ const test = require('ava');
 const PORT = 8567;
 
 async function createServer(port) {
-  app.use(serve(path.join(__dirname, '..')));
+  app.use(serve(join(__dirname, '..')));
 
   return new Promise((resolve, reject) => {
     const server = app.listen(port, () => resolve(server));
@@ -33,14 +33,14 @@ async function runPuppeteer(sd) {
 
   await page.mouse.click(200, 100, { clickCount: 2, delay: 50 });
 
-  const screenshot = path.join(sd, 'onePanel.png');
+  const screenshot = join(sd, 'onePanel.png');
 
   await page.screenshot({ path: screenshot });
 
   return new Promise((resolve, reject) => {
     looksSame(
       screenshot,
-      path.join(__dirname, '..', 'tests', 'fixtures', 'onePanel.png'),
+      join(__dirname, '..', 'tests', 'fixtures', 'onePanel.png'),
       { tolerance: 20, ignoreCaret: true },
       (error, equal) => {
         if (error) {
@@ -56,7 +56,7 @@ async function runPuppeteer(sd) {
 }
 
 test('simple', async t => {
-  const sd = path.join(__dirname, '..', 'build');
+  const sd = join(__dirname, '..', 'build');
   await makeDir(sd);
   const server = await createServer(PORT);
   const same = await runPuppeteer(sd);
